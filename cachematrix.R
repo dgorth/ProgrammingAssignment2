c71b8f6023c961648daa05a54faf5ab0bf0a3826
debdebdeb <63066519+dgorth@users.noreply.github.com>
2020-04-11 20:12:57 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions make a matrix with cached vaules
+## and solves for or calls the inverse
 
-## Write a short comment describing this function
+## further comments are included in code
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y 
+    inv <<- NULL ## initiates inv
+  }
+  get <- function() x ## prints matrix 
+  setInv <- function(solve) inv <<- solve 
+  getInv <- function() inv ## prints inv
+  list(set = set, ## names set() "set"
+       get = get, ## names get() "get"
+       setInv = setInv, ## names setInv "setInv"
+       getInv = getInv) ## names getInv "getInv"
 }
 
 
-## Write a short comment describing this function
+## This function either retrieves the cached value or 
+## if this value doesn't exists find inverse
+## further comments are included in code
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachSolve <- function(x, ...) {
+  m <- x$getInv() ## defines m as the value from matrix 
+  if(!is.null(m)) { ## checks if it exists
+    message("i got you") ## prints message 
+    return(m)
+  }
+  p <- x$get() ## retrieve the matrix
+  m <- solve(p) ## actually calcualte inverse here
+  x$setInv(m) ## define Inv as solution
+  m #prints m
 }
